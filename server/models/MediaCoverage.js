48e785846569bd5644a844accabaf15f8c680718
alfahadkhan120 <alfahadkhan120@gmail.com>
2026-05-28 20:58:48 +0530

    Update MediaCoverage model to include image field

--- server/models/MediaCoverage.js
+++ server/models/MediaCoverage.js
@@ -5,6 +5,7 @@ const mediaCoverageSchema = new mongoose.Schema({
   source: { type: String, required: [true, 'Source is required'], trim: true },
   url: { type: String, default: '' },
   logo: { type: String, default: '' },
+  image: { type: String, default: '' },
   date: { type: Date, default: Date.now },
   order: { type: Number, default: 0 },
 }, { timestamps: true });
