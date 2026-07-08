f9bd2fa890ab5e48238b5585c086b3000b0e8a1d
alfahadkhan120 <alfahadkhan120@gmail.com>
2026-07-08 21:35:55 +0530

    Restore standard api/index.js entry point post diagnostic check

--- api/index.js
+++ api/index.js
@@ -1,17 +1,2 @@
-let app;
-try {
-  app = require('../server/server.js');
-} catch (err) {
-  const express = require('express');
-  app = express();
-  app.all('*', (req, res) => {
-    res.status(500).json({
-      success: false,
-      error: 'Failed to initialize server',
-      message: err.message,
-      stack: err.stack,
-    });
-  });
-}
-
+const app = require('../server/server.js');
 module.exports = app;
